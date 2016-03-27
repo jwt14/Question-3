138d01db085e03f944d13303dd8e165a9d6fd64d
jwt14 <jwt14@ic.ac.uk>
2016-03-27 12:38:09 +0200

    v0.9
    Corrected RMS plotting in Python

--- q3d.py
+++ q3d.py
@@ -6,20 +6,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import pi,sin,exp,sqrt
 
-L = 1                                   #Any of these parameters can be changed by user
-N_x = range(20,2,-2)
+L = 1.                                   #Any of these parameters can be changed by user
+N_x = range(30,10,-2)
 T = 1
-N_t = range(1000,5500,200)
-theta = 0.5
-    #d_t = T/(N_t)                                      #Be careful! Integers could yield 0
+N_t = 500
+#N_t = range(1000,10000,1000)
+d_t = np.linspace(0.00001, 0.001, num=10)
+theta = 0
+                                     #Be careful! Integers could yield 0
 #d_x = L/float(N_x)
-alpha = 1
+alpha = 0.1
     
 RMSE_const_t= []
 RMSE_const_x = []
     
-for i in range(0,23):
-    data = subprocess.Popen("./q3 {} {} {} {} {} {}".format(L, N_x[0], T, N_t[i], alpha, theta),
+for i in range(0,10):
+    data = subprocess.Popen("./q3 {} {} {} {} {} {}".format(L, N_x[0], N_t*d_t[i], N_t, alpha, theta),
                             stdout=subprocess.PIPE).communicate()[0]
     a=data.split('\n')
     d_x = L/float(N_x[0])
@@ -27,16 +29,16 @@ for i in range(0,23):
     heat_analytic = []
     error = 0.
     for j in range(0, N_x[0]+1):
-        heat_vector.append(eval(a[int( (0.55*N_t[i]-1)*(N_x[0]+2) +1 +j)]))
-        heat_analytic.append(sin(pi*j*d_x/L)*exp(-alpha*pi**2*0.55/L/L))
-    
+        heat_vector.append(eval(a[int( (0.6*N_t-1)*(N_x[0]+2) +1 +j)]))
+        heat_analytic.append(sin(pi*j*d_x/L)*exp(-alpha*pi**2*(0.6*d_t[i]*N_t)/L/L))
+    print(i)
     for k in range(0, N_x[0]+1):
         error += (heat_analytic[k]-heat_vector[k])**2
     
     RMSE_const_x.append(sqrt(error/(N_x[0]+1)))
     
 for i in range(0,9):
-    data = subprocess.Popen("./q3 {} {} {} {} {} {}".format(L, N_x[i], T, N_t[0], alpha, theta),
+    data = subprocess.Popen("./q3 {} {} {} {} {} {}".format(L, N_x[i], T, N_t, alpha, theta),
                             stdout=subprocess.PIPE).communicate()[0]
     a=data.split('\n')
     d_x = L/float(N_x[i])
@@ -45,21 +47,22 @@ for i in range(0,9):
     heat_analytic = []
     error = 0.
     for j in range(0, N_x[i]+1):
-        heat_vector.append(eval(a[int( (0.55*N_t[0]-1)*(N_x[i]+2) +1 +j)]))
-        heat_analytic.append(sin(pi*j*d_x/L)*exp(-alpha*pi**2*0.55/L/L))
+        heat_vector.append(eval(a[int( (0.1*N_t-1)*(N_x[i]+2) +1 +j)]))
+        heat_analytic.append(sin(pi*j*d_x/L)*exp(-alpha*pi**2*0.1/L/L))
     
     for k in range(0, N_x[i]+1):
         error += (heat_analytic[k]-heat_vector[k])**2
     
     RMSE_const_t.append(sqrt(error/(N_x[i]+1)))
+d_x = np.linspace(1/30., 1/10., num=9)
 
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-axes[0].plot(N_t, RMSE_const_x)
+axes[0].plot(d_t, RMSE_const_x)
 axes[0].set_title("Changing time step")
-axes[0].set_xlabel('Number of time steps')
+axes[0].set_xlabel('Time step size')
 axes[0].set_ylabel('RMS Error')
 
-axes[1].plot(N_x, RMSE_const_t, 'r')
+axes[1].plot(d_x, RMSE_const_t, 'r')
 axes[1].set_title("Changing space step")
 axes[1].set_xlabel('Number of space steps')
 
